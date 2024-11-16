2fa6a2e612cbeb40a351ef108c1d2768b3e7c0c8
IEduStu <125146135+IEduStu@users.noreply.github.com>
2024-11-16 16:37:14 +0000

    feat: create aio

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM node:22-alpine as stremio-web
+
+RUN apk add --no-cache git
+
+WORKDIR /stremio-web
+
+ARG WEB_GIT_REPO="https://github.com/Stremio/stremio-web.git"
+ARG WEB_GIT_BRANCH="development"
+RUN git clone --single-branch --branch --depth 1 "$WEB_GIT_BRANCH" "$WEB_GIT_REPO" .
+
+RUN npm ci
+RUN npm run build
+
+FROM stremio/server:latest
+
+COPY --from=stremio-web /stremio-web/build /stremio-web
+
+ENV NO_CORS=1
+
+RUN mkdir -p /stremio-aio
+WORKDIR /stremio-aio
+COPY . .
+RUN npm ci
+
+EXPOSE 8080
+CMD ["node", "index.js"]
---
+++ index.js
@@ -0,0 +1,112 @@
+const path = require("path");
+const child_process = require("child_process");
+const http = require("http");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
+const INDEX_CACHE = 7200;
+const ASSETS_CACHE = 2629744;
+const AIO_HTTP_PORT = 8080;
+const SERVER_PORT = 11470;
+
+const webBuildPath = "/stremio-web";
+const indexPath = path.join(webBuildPath, "index.html");
+
+const app = express();
+
+app.get("/", cookieParser(), (req, res) => {
+    res.set("cache-control", `public, max-age: ${INDEX_CACHE}`);
+
+    if (req.cookies["stremio-server-set"] != "true") {
+        res.cookie("stremio-server-set", "true");
+
+        const currentUrl = new URL(req.originalUrl, `${req.protocol}://${req.get("host") || "localhost"}${req.originalUrl}`);
+        if (!currentUrl.searchParams.has("streamingServerUrl")) {
+            const newUrl = new URL(currentUrl.href);
+            newUrl.searchParams.set("streamingServerUrl", currentUrl.origin);
+
+            res.redirect(newUrl.href);
+            return;
+        }
+    }
+
+    res.sendFile(indexPath);
+});
+
+app.use(
+    express.static(webBuildPath, {
+        setHeaders(res, path) {
+            if (path === indexPath)
+                res.set("cache-control", `public, max-age: ${INDEX_CACHE}`);
+            else
+                res.set("cache-control", `public, max-age: ${ASSETS_CACHE}`);
+        }
+    })
+);
+
+const serverProcess = child_process.spawn("node", ["/stremio/server.js"], {
+    stdio: "inherit",
+    cwd: "/stremio"
+});
+serverProcess.on("exit", (code) => {
+    console.error(`Server process exited with code: ${code}`);
+    process.exit(code);
+});
+if (serverProcess.exitCode != null) {
+    console.error(`Server process exited with code: ${serverProcess.exitCode}`);
+    process.exit(serverProcess.exitCode);
+}
+
+app.use((req, res) => {
+    const request = http.request({
+        host: "127.0.0.1",
+        port: SERVER_PORT,
+        path: req.originalUrl,
+        method: req.method,
+        headers: req.headers
+    }, (reqRes) => {
+        res.writeHead(reqRes.statusCode, reqRes.headers);
+        reqRes.pipe(res, {
+            end: true
+        });
+
+        reqRes.on("error", (err) => {
+            console.error("Request response error:", err);
+
+            res
+                .status(500)
+                .write(err.message)
+                .end();
+        });
+    });
+
+    request.on("error", (err) => {
+        console.error("Request error:", err);
+
+        res
+            .status(500)
+            .write(err.message)
+            .end();
+    });
+
+    req.pipe(request, {
+        end: true
+    });
+
+    req.on("close", () => {
+        if (!request.closed)
+            request.destroy();
+    });
+});
+
+app.all("*", (req, res) => {
+    res.status(404).send("<h1>Page not found</h1>");
+});
+
+process.on("uncaughtException", (err) => {
+    console.error("Uncaught exception:", err);
+});
+
+app.listen(AIO_HTTP_PORT, () => {
+    console.info(`AIO server listening on port: ${AIO_HTTP_PORT}`)
+});
