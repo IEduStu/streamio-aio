faefb26d6ab1ab434209d3c44f1554ed0bcb059b
IEduStu <125146135+IEduStu@users.noreply.github.com>
2024-11-16 17:54:53 +0000

    fix: use relative `streamingServerUrl` query param

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ app.get("/", cookieParser(), (req, res) => {
     if (req.cookies["stremio-server-set"] != "true") {
         res.cookie("stremio-server-set", "true");
 
-        const currentUrl = new URL(req.originalUrl, `${req.protocol}://${req.get("host") || "localhost"}${req.originalUrl}`);
+        const currentUrl = new URL(req.originalUrl, "http://localhost:" + SERVER_PORT);
         if (!currentUrl.searchParams.has("streamingServerUrl")) {
             const newUrl = new URL(currentUrl.href);
-            newUrl.searchParams.set("streamingServerUrl", currentUrl.origin);
+            newUrl.searchParams.set("streamingServerUrl", "/");
 
             res.redirect(newUrl.href.slice(newUrl.origin.length));
             return;
